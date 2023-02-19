12809edea3a4d3c72ef7e683af727c792bc4f0d6
kulibinRuslan <kylibin_rysik@vk.com>
2023-02-20 00:44:52 +0300

    Git cache cleared

--- test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Registation_1 = require("./src/utils/whatsapp/Registation");
-new Registation_1.Registration().regNumber();
-// ================================================================
-// server.ts (Node.js (Express) backend)
-// import express from 'express';
-// const app = express();
-// app.listen(3000);
